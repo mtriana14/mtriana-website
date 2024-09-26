db5635e1ba5a72a51270ba0bbc38100652ad2062
mtriana14 <159511457+mtriana14@users.noreply.github.com>
2024-09-26 17:26:14 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'src/App.jsx', // Ensure this points to your main entry file
+      input: './index.html', // Ensure this points to your main entry file
     },
   },
 });
